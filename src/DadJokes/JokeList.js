012306b6b82d69354bed70b588ae9feb614ccbea
Nishant <menishant77@gmail.com>
2020-03-08 00:54:07 +0530

    Added Local Storage Functionality

--- src/DadJokes/JokeList.js
+++ src/DadJokes/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import Joke from "./Jokes";
+import { v4 as uuidv4 } from "uuid";
 import "./JokeList.css";
 const API_URL = "https://icanhazdadjoke.com/";
 
@@ -7,21 +9,46 @@ class JokeList extends Component {
 	static defaultProps = {
 		numberOfJokes: 10
 	};
+	//
 	constructor(props) {
 		super();
-		this.state = { jokes: [] };
+		this.state = {
+			jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+		};
 	}
-	async componentDidMount() {
+	//
+	componentDidMount() {
+		if (this.state.jokes.length === 0) this.getJokes();
+	}
+	//
+	getJokes = async () => {
 		let jokesArr = [];
 		while (jokesArr.length < this.props.numberOfJokes) {
 			let response = await Axios.get(API_URL, {
 				headers: { Accept: "application/json" }
 			});
 			if (response.status === 200)
-				jokesArr.push({ joke: response.data.joke, votes: 0 });
+				jokesArr.push({ id: uuidv4(), text: response.data.joke, votes: 0 });
 		}
-		this.setState({ jokes: jokesArr });
-	}
+		this.setState(
+			st => ({ jokes: [...st.jokes, ...jokesArr] }),
+			() =>
+				window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+		);
+	};
+	//
+	handleVotes = (id, delta) => {
+		this.setState(
+			st => ({
+				jokes: st.jokes.map(joke =>
+					joke.id === id ? { ...joke, votes: joke.votes + delta } : joke
+				)
+			}),
+			() =>
+				window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+		);
+	};
+	//
 	render() {
 		return (
 			<div className="JokeList">
@@ -33,13 +60,19 @@ class JokeList extends Component {
 						src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
 						alt="Laughing Emoji"
 					/>
-					<button className="Button">New Jokes</button>
+					<button className="Button" onClick={this.getJokes}>
+						New Jokes
+					</button>
 				</div>
 				<div className="JokeList-jokes">
 					{this.state.jokes.map(joke => (
-						<p>
-							{joke.joke} - {joke.votes}
-						</p>
+						<Joke
+							key={joke.id}
+							votes={joke.votes}
+							text={joke.text}
+							upvote={() => this.handleVotes(joke.id, 1)}
+							downvote={() => this.handleVotes(joke.id, -1)}
+						/>
 					))}
 				</div>
 			</div>
